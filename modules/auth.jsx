c09af61dcc392cd7f6df29a217c48d44fdfbab20
vito3315 <marusiaqwerti@yandex.ru>
2023-09-28 19:37:31 +0400

    111

--- modules/auth.jsx
+++ modules/auth.jsx
@@ -63,7 +63,7 @@ export default function AuthPage(){
             priority={true}
           />
 
-          <MyTextInput label="Номер телефона" type={'text'} value={myLogin} onChange={ e => setMyLogin(e.target.value) } />
+          <MyTextInput label="Номер телефона 11" type={'text'} value={myLogin} onChange={ e => setMyLogin(e.target.value) } />
           <MyTextInput label="Пароль" type={'password'} value={myPWD} onChange={ e => setMyPWD(e.target.value) } onKeyPress={ () => loginFN() } />
           
           <span>{loginErr}</span>
