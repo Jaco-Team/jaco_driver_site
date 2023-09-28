5628d510ce906922cd3df21e81d929e1ce5851dc
vito3315 <marusiaqwerti@yandex.ru>
2023-09-28 19:26:56 +0400

    111

---
+++ ecosystem.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    apps: [
+      {
+        name: 'driver_site',
+        exec_mode: 'cluster',
+        instances: 'max', 
+        script: 'node_modules/next/dist/bin/next',
+        args: 'start',
+        env_local: {
+          APP_ENV: 'local' // APP_ENV=local
+        },
+        env_development: {
+          APP_ENV: 'dev' // APP_ENV=dev
+        },
+        env_production: {
+          APP_ENV: 'prod' // APP_ENV=prod
+        }
+      }
+    ],
+
+    deploy : {
+        production : {
+          user : 'root',
+          host : '134.0.118.222',
+          ref  : 'main',
+          repo : 'git@github.com:vito3315/jaco_driver_site.git',
+          path : '/root/jaco_driver_site',
+          'pre-deploy-local': '',
+          'post-deploy' : 'pm2 reload all',
+          'pre-setup': ''
+        }
+      }
+  }
