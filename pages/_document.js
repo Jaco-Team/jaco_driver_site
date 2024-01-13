9235a360a339e3b7b18475f9c3c51a496b425765
vito3315 <marusiaqwerti@yandex.ru>
2024-01-13 21:25:47 +0400

    111

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default function Document() {
   return (
     <Html lang="ru" data-scroll="0">
       <Head />
-      <script src="https://api-maps.yandex.ru/2.1/?apikey=e4836399-cf83-49a0-806f-abf5ab552228&lang=ru_RU" type="text/javascript"></script>
+      <script src="https://api-maps.yandex.ru/2.1/?apikey=a94b4ec2-1216-48fa-a6a6-0f7cca9b4135&lang=ru_RU" type="text/javascript"></script>
       <script src="https://yookassa.ru/checkout-widget/v1/checkout-widget.js"></script>
       <body className={roboto.variable}>
         <Main />
